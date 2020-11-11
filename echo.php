f834fff908b9875297e0b01397aac3f5ad3149da
Alexander Anchishkin <alexanderanchishkin@gmail.com>
2020-11-11 13:47:41 +0300

    Добавил вчерашние файлы

---
+++ echo.php
@@ -0,0 +1,2 @@
+<?php
+	echo $_POST['name'] . ' ' . $_POST['lastName'];
---
+++ index.js
@@ -0,0 +1,159 @@
+$(function() {
+	console.log("jQuery is ready!");
+
+let interval;
+
+function getRandomNumber(a, b)
+{
+	let r = Math.random();
+	let maxR = b - a;
+
+	let randomNumber = a + Math.round(r * maxR);
+	return randomNumber;
+}
+
+function printRandom()
+{
+	let randomNumber = document.querySelector("#randomNumber");
+	randomNumber.innerHTML = getRandomNumber(1, 100);
+}
+
+function clickRandom()
+{
+	console.log("Кнопка нажата, интервал остановлен");
+	clearInterval(interval);
+}
+
+function clickTransit()
+{
+	//document.location.href = "/second.html";
+
+	let name = $(".name-field").val();
+	let lastName = $(".lastname-field").val();
+
+	console.log(name);
+	console.log(lastName);
+
+	$.ajax({
+		url: "http://web-site/echo.php",
+		async: true,
+		method: "POST",
+		data: {
+			"name": name,
+			"lastName": lastName
+		},
+		dataType: "json",
+	}).fail(function(response) {
+		console.log('fail');
+	}).done(function(response) {
+			console.log('success');
+			console.log(response);
+		});
+}
+
+function changeTextSize(newSize)
+{
+	let p = document.querySelector(".abc p");
+	p.style.fontSize = newSize + "px";
+}
+
+function main()
+{
+	let helloH1 = document.getElementById("hello");
+
+	let button = document.createElement("button");
+	$(button).html("Отправить");
+
+	document.body.appendChild(button);
+
+	console.log(button);
+	//button.addEventListener("click", clickTransit);
+	$(button).on("click", clickTransit);
+
+	//           0         1 		2			3			4		5
+	let a = ["Первый", "Второй", "Третий", "Четвёртый", "Пятый", "Шестой"];
+
+	let b = {
+		property: "value"
+	};
+
+	let arr = [
+		{
+			name: "Вася",
+			lastName: "Иванов"
+		},
+		{
+			name: "Петя",
+			lastName: "Петров"
+		},
+		{
+			name: "Ваня",
+			lastName: "ААА"
+		},
+		{
+			name: "333",
+			lastName: "БББ"
+		},
+		{
+			name: "444",
+			lastName: "111"
+		},
+		{
+			name: "555",
+			lastName: "222"
+		}
+	];
+
+	// <p>Первый</p>
+	// <p>Второй</p>
+	// <p>Третий</p>
+
+	/*for (let i = 0; i < arr.length; i++)
+	{
+		let p = document.createElement("p");
+		p.innerHTML = arr[i].name + " " + arr[i].lastName;
+		document.body.appendChild(p);
+	}*/
+
+	function aa(element)
+	{
+		let p = document.createElement("p");
+		p.innerHTML = element.name + " " + element.lastName;
+		document.body.appendChild(p);
+	};
+
+	arr.forEach(aa);
+/*
+	interval = setInterval(function() {
+		//printRandom();
+		changeTextSize(getRandomNumber(15, 70));
+	}, 500);
+
+	setTimeout(function() {
+		clickRandom();
+	}, 6000);
+
+*/
+
+	//document.body.removeChild(document.querySelector("button"));
+
+
+
+	// helloH1.innerHTML = "Пока!";
+	//
+
+	//let body = document.getElementsByTagName("body")[0];
+	//console.log(body);
+
+	//document.getElementById
+	//document.getElementsByTagName
+	//document.getElementsByClassName
+
+
+
+	//console.log(p);
+}
+
+main();
+
+});
