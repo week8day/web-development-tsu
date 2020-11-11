b2fc7f711e960df185e9ffd872a6debe86b495a4
KPolechka <74297616+KPolechka@users.noreply.github.com>
2020-11-11 18:38:04 +0500

    Добавила приветствие

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
 		<li><a href="/github/Lihoradka/index.html>Мария</a></li>
 		<li><a href="/github/IvanShkunov">Иван</a></li>
 		<li><a href="/github/DmitriyWW">Дмитрий мл</a></li>
+		<li><a href="/github/KPolechka">Полина Уфа</a></li>
 
 	</ul>
 	<ul>
