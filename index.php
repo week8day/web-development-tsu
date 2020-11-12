8635dc894166a703c7a8cbea01affe6399c3e65f
Alexander <master-004@mail.ru>
2020-11-12 14:50:30 +0800

    привет от Александра

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
 	<h1>СТРАНИЦЫ СЛУШАТЕЛЕЙ</h1>
 	<ul>
 		<li><a href="/github/alexanderanchishkin">Александр</a></li>
+		<li><a href="/github/oreolka">Александр</a></li>
 		<li><a href="/github/albert-garipov">Альберт</a></li>
 		<li><a href="/github/Un-R">Un-R</a></li>
 		<li><a href="/github/andreev/index.html">Form from fcc</a></li>
