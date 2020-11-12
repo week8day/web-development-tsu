318c6c75d1f08996d9ba50c03a66b8a461dcd144
Un-R <74297593+Un-R@users.noreply.github.com>
2020-11-12 11:20:57 +0300

    Поменял имя

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 		<li><a href="/github/alexanderanchishkin">Александр</a></li>
 		<li><a href="/github/oreolka/index.html">Александр</a></li>
 		<li><a href="/github/albert-garipov">Альберт</a></li>
-		<li><a href="/github/Un-R">Un-R</a></li>
+		<li><a href="/github/Un-R">Павел</a></li>
 		<li><a href="/github/andreev/index.html">Form from fcc</a></li>
 		<li><a href="/github/artemprovotorov">Артем</a></li>
 		<li><a href="/github/MrG-Vladimir">Владимир</a></li>
